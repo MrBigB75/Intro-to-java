888da47c34ca6fcb7e12a8195b7a9b52619c2517
MrBigB75 <105227555+MrBigB75@users.noreply.github.com>
2022-06-24 10:37:51 +0100

    Add files via upload

---
+++ prompt.js
@@ -0,0 +1,7 @@
+let person = {
+    name : "Iyke",
+    height : 6.1,
+    country : "Nigeria",
+}
+
+alert('My name is '+ person.name +'.\nI am '+ person.height + 'ft in height, and I am from ' + person.country +'.');
